1e097a51caf99c94e3bd2974368581dcf172f13b
Виктор <risk03corp@gmail.com>
2020-04-09 22:39:56 +0300

    users

--- src/main/java/bsuir/filipovich/accountingclient/controller/MainController.java
+++ src/main/java/bsuir/filipovich/accountingclient/controller/MainController.java
@@ -26,27 +26,22 @@ public class MainController {
 
     @RequestMapping(value = {"/users"}, method = RequestMethod.GET)
     public String viewUserList(Model model,
-                               @RequestParam(required = false) Integer id,
+                               @RequestParam(required = false) String id,
                                @RequestParam(required = false) String surname,
-                               @RequestParam(required = false) String add,
-                               @RequestParam(required = false) String save,
-                               @RequestParam(required = false) String delete,
+                               @RequestParam(required = false) String operation,
                                @RequestParam(required = false) String forename,
                                @RequestParam(required = false) String patronymic,
                                @RequestParam(required = false) String role,
                                @RequestParam(required = false) String login) {
         model.addAttribute("userList", service.getUserList());
-        if (add != null){
-            System.out.println("Надо записать");
-        }
-        else if (save != null){
-            System.out.println("Надо перезаписать");
-        }
-        else if (delete != null){
-            System.out.println("Надо удалить");
-        }
-        else{
+        if (operation == null) {
             System.out.println("Just Check-in");
+        } else if (operation.equals("Добавить")) {
+            service.create("user", new String[]{id, surname, forename, patronymic, role, login, "4138cb2035b332f60280335e2a3e8a37", "f8feb0246fc4889a02a56291eee6f08a"});
+        } else if (operation.equals("Сохранить")) {
+            service.update("user", new String[]{id, surname, forename, patronymic, role, login, null, null});
+        } else if (operation.equals("Удалить")) {
+            service.remove("user", id);
         }
         return "users";
     }
--- src/main/java/bsuir/filipovich/accountingclient/service/IService.java
+++ src/main/java/bsuir/filipovich/accountingclient/service/IService.java
@@ -3,7 +3,13 @@ package bsuir.filipovich.accountingclient.service;
 import java.util.ArrayList;
 
 public interface IService {
-    public ArrayList<String> getUserList();
+    public ArrayList<String[]> getUserList();
 
     public String getMessage();
+
+    void create(String type, String[] strings);
+
+    void update(String type, String[] strings);
+
+    void remove(String type, String id);
 }
